04ed67a679362738d196bed42adea64b10419f07
Patrick Krueger <sendthistopatrick@gmail.com>
2024-09-04 23:26:47 -0600

    add rename tab keybind

--- init.lua
+++ init.lua
@@ -115,8 +115,23 @@ config.keys = {
 		action = act.PromptInputLine({
 			description = "Enter new tab name",
 			action = wezterm.action_callback(function(window, pane, line)
-				if line then
+				if line and line ~= "" then
 					window:perform_action(act.SpawnTab("CurrentPaneDomain"), pane)
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line and line ~= "" then
 					window:active_tab():set_title(line)
 				end
 			end),
